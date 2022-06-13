77f134254ab4532ade37ae0123279ca8d910c76b
matteocovetti <covetti.matteo@gmail.com>
2022-06-13 11:20:43 +0200

    terzo snack completato

--- src/jsnacks/JSnack3.java
+++ src/jsnacks/JSnack3.java
@@ -1,11 +1,24 @@
 package jsnacks;
 
+import java.util.Random;
+
 public class JSnack3 {
 	// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
 	// lista di cognomi, genera una falsa lista di 3 invitati.
 
 	public static void main(String[] args) {
+		Random rand = new Random();
 		
+		String[] nomi = {"Matteo","Francesco","Giuseppe","Simone","Alessio"};
+		String[] cognomi = {"Rossi","Bianchi","Neri","Verdi","Covetti"};
+		
+		String PrimoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
+		String SecondoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
+		String TerzoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
+		System.out.println("I tre invitati sono: ");
+		System.out.println(PrimoInvitato);
+		System.out.println(SecondoInvitato);
+		System.out.println(TerzoInvitato);
 
 	}
 
