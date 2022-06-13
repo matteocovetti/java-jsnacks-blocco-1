07e0e01757598f367d12e1061583520de4b3f213
matteocovetti <covetti.matteo@gmail.com>
2022-06-13 12:52:46 +0200

    modifiche live coding snack3

--- src/jsnacks/JSnack3.java
+++ src/jsnacks/JSnack3.java
@@ -8,17 +8,36 @@ public class JSnack3 {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		
-		String[] nomi = {"Matteo","Francesco","Giuseppe","Simone","Alessio"};
-		String[] cognomi = {"Rossi","Bianchi","Neri","Verdi","Covetti"};
-		
-		String PrimoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
-		String SecondoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
-		String TerzoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
-		System.out.println("I tre invitati sono: ");
-		System.out.println(PrimoInvitato);
-		System.out.println(SecondoInvitato);
-		System.out.println(TerzoInvitato);
+
+		String[] nomi = { "Matteo", "Francesco", "Giuseppe", "Simone", "Alessio" };
+		String[] cognomi = { "Rossi", "Bianchi", "Neri", "Verdi", "Covetti" };
+
+//		String PrimoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
+//		String SecondoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
+//		String TerzoInvitato = nomi[rand.nextInt(nomi.length)] + " " + cognomi[rand.nextInt(cognomi.length)];
+//		System.out.println("I tre invitati sono: ");
+//		System.out.println(PrimoInvitato);
+//		System.out.println(SecondoInvitato);
+//		System.out.println(TerzoInvitato);
+
+		// live coding
+
+		String[] fake = new String[3];
+
+		for (int i = 0; i < fake.length; i++) {
+			// creo un indice casuale tra 0 e la lunghezza dell'array nomi
+			int index = rand.nextInt(nomi.length);
+			// prendo l'elemento dell'array nomi
+			String nomeCasule = nomi[index];
+			// creo un indice casuale tra 0 e la lunghezza dell'array cognomi
+			index = rand.nextInt(cognomi.length);
+			// prendo l'elemento dell'array cognomi
+			String cognomeCasuale = cognomi[index];
+
+			// metto il nome fake alla posizione corrente
+			fake[i] = nomeCasule + " " + cognomeCasuale;
+			System.out.println(fake[i]);
+		}
 
 	}
 
