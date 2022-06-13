8a966ffcacae5e026b1539c28fa1e4fe8b9cee03
matteocovetti <covetti.matteo@gmail.com>
2022-06-13 11:56:30 +0200

    inizio quarto snack

---
+++ src/jsnacks/JSnack4.java
@@ -0,0 +1,12 @@
+package jsnacks;
+
+public class JSnack4 {
+	//Crea un array di numeri interi
+	// e fai la somma di tutti gli elementi che sono in posizione dispari
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
