72d11e9d091fc4a887788b026238dd1c88cb1b93
matteocovetti <covetti.matteo@gmail.com>
2022-06-13 10:58:37 +0200

    inizio terzo snack

---
+++ src/jsnacks/JSnack3.java
@@ -0,0 +1,12 @@
+package jsnacks;
+
+public class JSnack3 {
+	// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+	// lista di cognomi, genera una falsa lista di 3 invitati.
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
