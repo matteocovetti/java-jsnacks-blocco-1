5e3a9db03b7982befd130da703c666861da526a5
matteocovetti <covetti.matteo@gmail.com>
2022-06-13 10:49:03 +0200

    inizio secondo snack

---
+++ src/jsnacks/JSnack2.java
@@ -0,0 +1,12 @@
+package jsnacks;
+
+public class JSnack2 {
+	// Chiedi all'utente di inserire un numero, se è pari stampa il numero,
+	// se è dispari stampa il numero successivo
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
