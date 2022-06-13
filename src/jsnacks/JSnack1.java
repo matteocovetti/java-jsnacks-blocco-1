02d843b5337517ea5f1818dd3ed2ce7bb6373cd5
matteocovetti <covetti.matteo@gmail.com>
2022-06-13 10:35:33 +0200

    first commit

---
+++ src/jsnacks/JSnack1.java
@@ -0,0 +1,14 @@
+package jsnacks;
+
+public class JSnack1 {
+
+	/*Il software deve chiedere per 5 volte all’utente di inserire un numero.
+	 * Il programma stampa la somma di tutti i numeri inseriti.
+	 * Esegui questo programma in due versioni, con il for e con il while.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
