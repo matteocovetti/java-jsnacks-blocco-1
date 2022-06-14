bda3d7be16abfdf0b1a9149ef2e151cf6bdae7b6
matteocovetti <covetti.matteo@gmail.com>
2022-06-14 09:32:58 +0200

    quarto snack finito - live coding

--- src/jsnacks/JSnack4.java
+++ src/jsnacks/JSnack4.java
@@ -1,11 +1,33 @@
 package jsnacks;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class JSnack4 {
-	//Crea un array di numeri interi
+	// Crea un array di numeri interi
 	// e fai la somma di tutti gli elementi che sono in posizione dispari
 
 	public static void main(String[] args) {
-		
+		// creo random
+		Random rand = new Random();
+
+		// creo un array di 50 elementi
+		int[] numeri = new int[50];
+
+		for (int i = 0; i < numeri.length; i++) {
+			numeri[i] = rand.nextInt(101);
+		}
+		System.out.println(Arrays.toString(numeri));
+
+		int somma = 0;
+		for (int i = 0; i < numeri.length; i++) {
+			// solo se indice dispari
+			if (i % 2 != 0) {
+				somma += numeri[i];
+			}
+		}
+
+		System.out.println("La somma degli elementi in posizione dispari è: " + somma);
 
 	}
 
---
+++ src/jsnacks/JSnack5.java
@@ -0,0 +1,14 @@
+package jsnacks;
+
+public class JSnack5 {
+	/*
+	 * La distanza di Hamming tra due stringhe di uguale lunghezza è il numero di
+	 * posizioni nelle quali i caratteri corrispondenti sono diversi. Chiedi
+	 * all'utente di dirti due parole e stampa la distanza di Hamming.
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
